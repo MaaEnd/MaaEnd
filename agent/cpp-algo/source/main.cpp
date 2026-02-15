f9e9eebedd6675bb352de2842b4aa699fe3436f7
MistEO <mistereo@hotmail.com>
2026-02-16 02:38:32 +0800

    feat: 跨目录复用 dll

--- agent/cpp-algo/source/main.cpp
+++ agent/cpp-algo/source/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
+#include "utils.h"
+
+int main()
+{
+#ifdef _WIN32
+    if (!setup_dll_directory()) {
+        std::cerr << "Warning: Failed to set DLL directory to maafw" << std::endl;
+    }
+#endif
+
     std::cout << "Hello, cpp-algo!" << std::endl;
     return 0;
 }
