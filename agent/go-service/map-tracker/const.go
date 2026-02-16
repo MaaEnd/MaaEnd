9e4fc4c83e3debf293ee754c73a4a391c9c7ef12
Harry Huang <harryhuang2652@qq.com>
2026-02-16 22:16:51 +0800

    feat(map): 添加MapTrackerMove自动寻路功能

--- agent/go-service/map-tracker/const.go
+++ agent/go-service/map-tracker/const.go
@@ -1,6 +1,11 @@
 // Copyright (c) 2026 Harry Huang
 package maptracker
 
+const (
+	WORK_W = 1280
+	WORK_H = 720
+)
+
 // Location inference configuration
 var (
 	// Mini-map crop area
@@ -21,4 +26,28 @@ var (
 const (
 	MAP_DIR      = "image/MapTracker/map"
 	POINTER_PATH = "image/MapTracker/pointer.png"
+)
+
+// Move action configuration
+const (
+	INFER_INTERVAL_MS                = 200
+	ARRIVAL_TOLERANCE                = 5  // Unit: mini-map pixel distance
+	ROTATION_TOLERANCE               = 10 // Unit: degree
+	ROTATION_SENSITIVITY             = 2.75
+	STUCK_MIN_DURATION_MS            = 1000
+	FAILURE_ARRIVAL_MAX_DURATION_MS  = 60000
+	FAILURE_ROTATION_MAX_DURATION_MS = 30000
+	FAILURE_STUCK_MAX_DURATION_MS    = 10000
+)
+
+// Win32 action related codes
+const (
+	KEY_W     = 0x57
+	KEY_A     = 0x41
+	KEY_S     = 0x53
+	KEY_D     = 0x44
+	KEY_SHIFT = 0x10
+	KEY_CTRL  = 0x11
+	KEY_ALT   = 0x12
+	KEY_SPACE = 0x20
 )
