c97720cf87a7ddaf0b29b3875d611cabc8ccb332
MistEO <mistereo@hotmail.com>
2026-02-21 16:48:01 +0800

    feat: 添加完整示例

--- agent/cpp-algo/source/main.cpp
+++ agent/cpp-algo/source/main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 
-#include "utils.h"
+#include <MaaAgentServer/MaaAgentServerAPI.h>
+#include <MaaToolkit/MaaToolkitAPI.h>
 
-int main()
+#include "utils.h"
+#include "my_reco_1/my_reco_1.h"
+
+int main(int argc, char** argv)
 {
 #ifdef _WIN32
-    if (!setup_dll_directory()) {
-        std::cerr << "Warning: Failed to set DLL directory to maafw" << std::endl;
-    }
+	if (!setup_dll_directory()) {
+		std::cerr << "Warning: Failed to set DLL directory to maafw" << std::endl;
+	}
 #endif
 
-    std::cout << "Hello, cpp-algo!" << std::endl;
-    return 0;
+	if (argc < 2) {
+		std::cerr << "Usage: cpp-algo <socket_id>" << std::endl;
+		std::cerr << "socket_id is provided by AgentIdentifier." << std::endl;
+		return -1;
+	}
+
+	std::cout << "Hello, cpp-algo!" << std::endl;
+
+	MaaToolkitConfigInitOption("./debug/cpp-algo", "{}");
+
+	MaaAgentServerRegisterCustomRecognition("MyReco1", ChildCustomRecognitionCallback, nullptr);
+
+	const char* identifier = argv[argc - 1];
+
+	MaaAgentServerStartUp(identifier);
+
+	MaaAgentServerJoin();
+
+	MaaAgentServerShutDown();
+
+	return 0;
 }
