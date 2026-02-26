5e58a477945e1440cdab652af1d4cb6f6816ad74
HYY1116 <hu1078725025@gmail.com>
2026-02-26 20:07:06 +0800

    refactor(resell):拆分resell.go，尽量单action单操作

--- agent/go-service/resell/register.go
+++ agent/go-service/resell/register.go
@@ -4,11 +4,17 @@ import "github.com/MaaXYZ/maa-framework-go/v4"
 
 var (
 	_ maa.CustomActionRunner = &ResellInitAction{}
+	_ maa.CustomActionRunner = &ResellCheckQuotaAction{}
+	_ maa.CustomActionRunner = &ResellScanProductAction{}
+	_ maa.CustomActionRunner = &ResellDecideAction{}
 	_ maa.CustomActionRunner = &ResellFinishAction{}
 )
 
 // Register registers all custom action components for resell package
 func Register() {
 	maa.AgentServerRegisterCustomAction("ResellInitAction", &ResellInitAction{})
+	maa.AgentServerRegisterCustomAction("ResellCheckQuotaAction", &ResellCheckQuotaAction{})
+	maa.AgentServerRegisterCustomAction("ResellScanProductAction", &ResellScanProductAction{})
+	maa.AgentServerRegisterCustomAction("ResellDecideAction", &ResellDecideAction{})
 	maa.AgentServerRegisterCustomAction("ResellFinishAction", &ResellFinishAction{})
 }
