27e373aa9184bab156315587538dfee496772ea5
jwtggg <1030562538@qq.com>
2026-03-07 09:58:58 +0800

    refactor:精简go代码，只留一个转换识别区域坐标的函数，其他功能改用pipeline实现

--- agent/go-service/autoecofarm/register.go
+++ agent/go-service/autoecofarm/register.go
@@ -3,12 +3,11 @@ package autoecofarm
 import "github.com/MaaXYZ/maa-framework-go/v4"
 
 var (
-	_ maa.CustomActionRunner = &MoveToTarget3D{}
-	_ maa.CustomActionRunner = &MoveToTargetPart{}
+	_ maa.CustomRecognitionRunner = &autoEcoFarmCalculateSwipeTarget{}
 )
 
 // Register registers the aspect ratio checker as a tasker sink
 func Register() {
-	maa.AgentServerRegisterCustomAction("MoveToTarget3D", &MoveToTarget3D{})
-	maa.AgentServerRegisterCustomAction("MoveToTargetPart", &MoveToTargetPart{})
+	maa.AgentServerRegisterCustomRecognition("autoEcoFarmCalculateSwipeTarget", &autoEcoFarmCalculateSwipeTarget{})
+
 }
