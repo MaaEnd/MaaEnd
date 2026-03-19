392d0e2064a2acdb5dcc68dc88748800466f2bd7
zmdyy0318 <zmdyy0318@hotmail.com>
2026-03-19 20:45:19 +0800

    fix:修复ai建议

--- agent/go-service/dailyrewards/register.go
+++ agent/go-service/dailyrewards/register.go
@@ -2,11 +2,6 @@ package dailyrewards
 
 import "github.com/MaaXYZ/maa-framework-go/v4"
 
-var (
-	_ maa.CustomRecognitionRunner = &DailyEventUnreadItemInitRecognition{}
-	_ maa.CustomActionRunner      = &DailyEventUnreadItemInitAction{}
-)
-
 // Register registers all custom recognition and action components for dailyrewards package
 func Register() {
 	maa.AgentServerRegisterCustomRecognition("DailyEventUnreadItemInitRecognition", &DailyEventUnreadItemInitRecognition{})
