5037ddc03a11a98d700393467186393b326dba86
overflow65537 <err.overflow@gmail.com>
2026-04-06 10:42:15 +0800

    perf: 添加注释以解释好友列表识别区域的魔法数字

--- agent/go-service/visitfriends/visitfriends.go
+++ agent/go-service/visitfriends/visitfriends.go
@@ -106,9 +106,9 @@ func getFriendItemsRoi(ctx *maa.Context, arg *maa.CustomRecognitionArg) ([]maa.R
 		}
 		box := detail.Box
 		rois = append(rois, maa.Rect{
-			8,
+			8, //魔法数字(
 			box.Y() - 30,
-			1205,
+			1205, //魔法数字(
 			box.Height() + 65,
 		}) // 识别区偏移为整个水平范围，保留原有高度映射
 	}
