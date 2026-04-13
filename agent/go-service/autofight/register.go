e490d36b2f9ae6fde61a6b70c15a0e1ca3c39a8e
zmdyy0318 <zmdyy0318@hotmail.com>
2026-04-13 14:47:13 +0800

    refactor: 重构自动战斗

--- agent/go-service/autofight/register.go
+++ agent/go-service/autofight/register.go
@@ -5,8 +5,5 @@ import "github.com/MaaXYZ/maa-framework-go/v4"
 // Register registers all custom recognition and action components for autofight package
 func Register() {
 	maa.AgentServerRegisterCustomRecognition("AutoFightEntryRecognition", &AutoFightEntryRecognition{})
-	maa.AgentServerRegisterCustomRecognition("AutoFightExitRecognition", &AutoFightExitRecognition{})
-	maa.AgentServerRegisterCustomRecognition("AutoFightPauseRecognition", &AutoFightPauseRecognition{})
-	maa.AgentServerRegisterCustomRecognition("AutoFightExecuteRecognition", &AutoFightExecuteRecognition{})
-	maa.AgentServerRegisterCustomAction("AutoFightExecuteAction", &AutoFightExecuteAction{})
+	maa.AgentServerRegisterCustomAction("AutoFightMainAction", &AutoFightMainAction{})
 }
