d81e17ae5c6878c533912672bb4286a8840b5f00
Harry Huang <harryhuang2652@qq.com>
2026-04-22 22:57:16 +0800

    perf: minicv模板匹配采用SIMD计算NCC

--- agent/go-service/pkg/minicv/match_template.go
+++ agent/go-service/pkg/minicv/match_template.go
@@ -7,6 +7,7 @@ import (
 	"math"
 	"os"
 	"sync"
+	"unsafe"
 )
 
 // Template represents a preloaded template image along with its integral array and statistics for matching.
@@ -98,6 +99,13 @@ func subpixelOffset(neg, pos float64) float64 {
 	return min(1.0, max(-1.0, offset))
 }
 
+func dotRGBA3(imgPix, tplPix *uint8, pixels int) uint64 {
+	if pixels <= 0 {
+		return 0
+	}
+	return dotRGBA3Impl(unsafe.Pointer(imgPix), unsafe.Pointer(tplPix), pixels)
+}
+
 // ComputeNCC computes the normalized cross-correlation between a rectangle region in the haystack image
 // and a template image, using precomputed integral array for efficiency
 func ComputeNCC(img *image.RGBA, imgIntArr IntegralArray, tpl *image.RGBA, tplStats StatsResult, ox, oy int) float64 {
@@ -111,18 +119,12 @@ func ComputeNCC(img *image.RGBA, imgIntArr IntegralArray, tpl *image.RGBA, tplSt
 	tpx, ts := tpl.Pix, tpl.Stride
 
 	var dot uint64
-	iOffBase := oy*is + ox*4
-	for y := range th {
-		iOff := iOffBase
-		tOff := y * ts
-		for range tw {
-			dot += uint64(ipx[iOff]) * uint64(tpx[tOff])
-			dot += uint64(ipx[iOff+1]) * uint64(tpx[tOff+1])
-			dot += uint64(ipx[iOff+2]) * uint64(tpx[tOff+2])
-			iOff += 4
-			tOff += 4
-		}
-		iOffBase += is
+	iOff := oy*is + ox*4
+	tOff := 0
+	for range th {
+		dot += dotRGBA3(&ipx[iOff], &tpx[tOff], tw)
+		iOff += is
+		tOff += ts
 	}
 
 	count := float64(tw * th * 3)
@@ -174,16 +176,7 @@ func ComputeNCCInCircle(
 
 		iOff := (oy+ty)*is + (ox+x0)*4
 		tOff := ty*ts + x0*4
-
-		for x := x0; x <= x1; x++ {
-			ir, ig, ib := uint64(ipx[iOff]), uint64(ipx[iOff+1]), uint64(ipx[iOff+2])
-			tr, tg, tb := uint64(tpx[tOff]), uint64(tpx[tOff+1]), uint64(tpx[tOff+2])
-
-			dot += ir*tr + ig*tg + ib*tb
-
-			iOff += 4
-			tOff += 4
-		}
+		dot += dotRGBA3(&ipx[iOff], &tpx[tOff], width)
 	}
 
 	count := float64(pixelCount * 3)
