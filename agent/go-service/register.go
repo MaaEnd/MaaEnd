ed354ea608b36eb4c7e6ecf239b67f9bb24ac114
overflow65537 <err.overflow@gmail.com>
2026-05-14 22:05:23 +0800

    Revert "feat:信用点商店增加记录模块"
    This reverts commit 89687b97520faa930271fbd12d89492083a24c2b.

--- agent/go-service/register.go
+++ agent/go-service/register.go
@@ -8,7 +8,6 @@ import (
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/autostockstaple"
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/batchaddfriends"
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/bettersliding"
-	"github.com/MaaXYZ/MaaEnd/agent/go-service/creditshopping"
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/blueprintimport"
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/common/attachregex"
 	"github.com/MaaXYZ/MaaEnd/agent/go-service/common/autoaltclick"
@@ -73,7 +72,6 @@ func registerAll() {
 	autostockpile.Register()
 	itemtransfer.Register()
 	sellproduct.Register()
-	creditshopping.Register()
 	log.Info().
 		Msg("All custom components and sinks registered successfully")
 }
