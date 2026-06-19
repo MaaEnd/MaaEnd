75a876b99a9678f899aa3c23c777c13085202738
overflow65537 <err.overflow@gmail.com>
2026-06-19 23:30:26 +0800

    这次应该对了

--- agent/go-service/common/autoalt/swipe.go
+++ agent/go-service/common/autoalt/swipe.go
@@ -7,32 +7,26 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type autoAltSwipeParam struct {
-	// Begin overrides the swipe start. When omitted, the sub-node Swipe action
-	// uses arg.Box as begin (Swipe default begin=true).
-	Begin []int `json:"begin,omitempty"`
-	// End overrides the swipe end. When omitted, the sub-node Swipe action
-	// uses arg.Box as end (Swipe default end=true).
-	End []int `json:"end,omitempty"`
-	// BeginOffset is forwarded to the sub-node Swipe action's begin_offset.
-	BeginOffset []int `json:"begin_offset,omitempty"`
-	// EndOffset is forwarded to the sub-node Swipe action's end_offset.
-	EndOffset []int `json:"end_offset,omitempty"`
-	Duration  *int  `json:"duration,omitempty"`
-	EndHold   *int  `json:"end_hold,omitempty"`
-	OnlyHover *bool `json:"only_hover,omitempty"`
-}
+const autoAltSwipeSubNode = "__AutoAltSwipeMouseSwipeAction"
 
 type AutoAltSwipeAction struct{}
 
 // Compile-time interface check
 var _ maa.CustomActionRunner = &AutoAltSwipeAction{}
 
+func rectToSlice(box maa.Rect) []int {
+	return []int{box[0], box[1], box[2], box[3]}
+}
+
 func (a *AutoAltSwipeAction) Run(ctx *maa.Context, arg *maa.CustomActionArg) bool {
-	swipeOverride := map[string]any{}
+	box := rectToSlice(arg.Box)
+	swipeOverride := map[string]any{
+		"begin": box,
+		"end":   box,
+	}
 	if param := arg.CustomActionParam; param != "" {
-		var p autoAltSwipeParam
-		if err := json.Unmarshal([]byte(param), &p); err != nil {
+		var customParam map[string]any
+		if err := json.Unmarshal([]byte(param), &customParam); err != nil {
 			log.Error().
 				Err(err).
 				Str("component", "AutoAltSwipeAction").
@@ -40,26 +34,8 @@ func (a *AutoAltSwipeAction) Run(ctx *maa.Context, arg *maa.CustomActionArg) boo
 				Msg("failed to parse custom action param")
 			return false
 		}
-		if len(p.Begin) == 2 || len(p.Begin) == 4 {
-			swipeOverride["begin"] = p.Begin
-		}
-		if len(p.End) == 2 || len(p.End) == 4 {
-			swipeOverride["end"] = p.End
-		}
-		if len(p.BeginOffset) == 4 {
-			swipeOverride["begin_offset"] = p.BeginOffset
-		}
-		if len(p.EndOffset) == 4 {
-			swipeOverride["end_offset"] = p.EndOffset
-		}
-		if p.Duration != nil {
-			swipeOverride["duration"] = *p.Duration
-		}
-		if p.EndHold != nil {
-			swipeOverride["end_hold"] = *p.EndHold
-		}
-		if p.OnlyHover != nil {
-			swipeOverride["only_hover"] = *p.OnlyHover
+		for k, v := range customParam {
+			swipeOverride[k] = v
 		}
 	}
 
@@ -72,21 +48,16 @@ func (a *AutoAltSwipeAction) Run(ctx *maa.Context, arg *maa.CustomActionArg) boo
 		return false
 	}
 
-	var swipeErr error
-	// begin/end default to arg.Box via sub-node Swipe (begin/end=true); param fields override when set.
-	if len(swipeOverride) > 0 {
-		_, swipeErr = ctx.RunAction("__AutoAltSwipeMouseSwipeAction",
-			arg.Box, "", map[string]any{
-				"__AutoAltSwipeMouseSwipeAction": swipeOverride,
-			})
-	} else {
-		_, swipeErr = ctx.RunAction("__AutoAltSwipeMouseSwipeAction",
-			arg.Box, "", nil)
-	}
+	_, swipeErr := ctx.RunAction(autoAltSwipeSubNode,
+		arg.Box, "", map[string]any{
+			autoAltSwipeSubNode: swipeOverride,
+		})
 	if swipeErr != nil {
 		log.Error().
 			Err(swipeErr).
 			Str("component", "AutoAltSwipeAction").
+			Interface("box", arg.Box).
+			Interface("swipe_override", swipeOverride).
 			Msg("failed to run __AutoAltSwipeMouseSwipeAction")
 	}
 
