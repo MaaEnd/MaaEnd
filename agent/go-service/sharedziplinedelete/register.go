5cf932e9a33ad9c749d9744ca321215989a23c52
zmdyy0318 <zmdyy0318@hotmail.com>
2026-09-20 21:13:12 +0800

    使用mapfind

--- agent/go-service/sharedziplinedelete/register.go
+++
@@ -1,7 +0,0 @@
-package sharedziplinedelete
-
-import maa "github.com/MaaXYZ/maa-framework-go/v4"
-
-func Register() {
-	maa.AgentServerRegisterCustomRecognition(componentName, &HitRecognition{})
-}
